c4373c48dc235335927a913f4de355a36a1a130b
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-10-25 20:54:32 +0600

    course details page design updated with PDF button and footer added

--- src/components/Shared/CourseDetails/CourseDetails.js
+++ src/components/Shared/CourseDetails/CourseDetails.js
@@ -4,22 +4,38 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import { FaEye, } from "react-icons/fa";
-import { FaClock, FaUserAlt, FaStar } from "react-icons/fa";
-import './CourseDetails.css'
+import { FaClock, FaUserAlt, FaStar, FaArrowDown } from "react-icons/fa";
+import './CourseDetails.css';
+import Table from 'react-bootstrap/Table';
+
+
+
 
 const CourseDetails = () => {
 
     const course = useLoaderData();
-    const { id, course_id, course_name, rating, students, course_image, course_description, duration, course_fee, instructor } = course;
+    const { id, course_id, course_name, rating, students, course_image, course_description, duration, course_fee, instructor, specialty1, specialty2, specialty3, specialty4, specialty5, specialty6 } = course;
     const { name, instructor_img } = course.instructor
     console.log(course);
 
     return (
         <div>
-            <Card className='container mt-5 w-75 border border-0'>
+            <Card className='container mt-3 w-75 border border-0'>
 
                 <Card.Body className='border border-2 border-primary mt-3 course-card-body rounded-3'>
-                    <Card.Header as="h3" className='text-white text-center border border-2 border-dark rounded-3 course-title-name mb-3'>{course_name}</Card.Header>
+                    <div className=''>
+                        {/* <Card.Header as="h3" className='text-white text-center border border-2 border-dark rounded-3 course-title-name mb-3'>{course_name} <Button className=''><FaArrowDown></FaArrowDown>Download</Button></Card.Header> */}
+
+                        <Card.Header as="h4" className='text-white text-center border border-2 border-dark rounded-3 course-title-name mb-3'>
+                            <div className='d-flex align-items-center justify-content-center flex-column flex-lg-row flex-md-row'>
+                                {course_name}
+                                <Button className='ms-3 mt-2' variant="success"><FaArrowDown className='me-2'></FaArrowDown>Download PDF</Button>
+                            </div>
+
+                        </Card.Header>
+
+                    </div>
+
                     <div className='d-flex align-items-center'>
                         <Card.Title className='me-2'>Enjoy {course_name} with {name}</Card.Title>
                         <Image
@@ -34,17 +50,51 @@ const CourseDetails = () => {
                         <div>
                             <p className=''><span className='fw-bold'>Description:</span> <span className=''>{course_description}</span></p>
                         </div>
-                        <div className='d-flex align-items-center'>
-                            <FaClock></FaClock>
-                            <p className="card-text ms-2"><span className='fw-bold'>Duration:</span> {duration}</p>
+
+                        <div className='d-flex align-items-center justify-content-around mb-4 mt-2 flex-column flex-lg-row flex-md-row'>
+                            <div className='d-flex align-items-center duration-div px-4 py-2  rounded-2 mb-2'>
+                                <FaClock></FaClock>
+                                <p className="card-text ms-2"><span className='fw-bold'>Duration:</span> {duration}</p>
+                            </div>
+                            <div className='d-flex align-items-center students-div px-4 py-2  rounded-2 mb-2'>
+                                <FaUserAlt></FaUserAlt>
+                                <p className="card-text ms-2"><span className='fw-bold'>Students: </span>{students}</p>
+                            </div>
+                            <div className='d-flex align-items-center rating-div px-4 py-2  rounded-2 mb-2'>
+                                <p className='mb-0'><span className='fw-bold'>Ratings:</span> {rating.number}</p>
+                                <FaStar className='text-warning me-2 ms-2'></FaStar>
+                            </div>
                         </div>
-                        <div className='d-flex align-items-center'>
-                            <FaUserAlt></FaUserAlt>
-                            <p className="card-text ms-2"><span className='fw-bold'>Students: </span>{students}</p>
-                        </div>
-                        <div className='d-flex align-items-center'>
-                            <p className='mb-0'><span className='fw-bold'>Ratings:</span> {rating.number}</p>
-                            <FaStar className='text-warning me-2'></FaStar>
+
+                        <div className='mx-auto container w-75 mb-3'>
+                            <Table striped bordered hover variant="light" className='text-center border border-1 fw-bold'>
+                                <thead>
+                                    <tr>
+                                        <th className='fs-5 text-primary'>Course Specialty</th>
+                                    </tr>
+                                </thead>
+                                <tbody className='fw-bold'>
+                                    <tr>
+                                        <td>{specialty1}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{specialty2}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{specialty3}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>{specialty4}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{specialty5}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{specialty6}</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
                         </div>
                     </Card.Text>
                     <Card.Footer as="h4" className="text-center text-white border border-2 border-dark course-card-footer rounded-3">Course Fee : {course_fee}</Card.Footer>
