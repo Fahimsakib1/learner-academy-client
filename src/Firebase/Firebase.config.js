4925dff0028e21d3446ef840a20f151c17a68852
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-10-26 00:36:47 +0600

    set up the environment variable

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA0mzsWA2XAVfcnwKk7r-hT1AQf4dmOfH4",
-    authDomain: "learner-academy.firebaseapp.com",
-    projectId: "learner-academy",
-    storageBucket: "learner-academy.appspot.com",
-    messagingSenderId: "978214749188",
-    appId: "1:978214749188:web:aa9c65ebd62e42600be151"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
